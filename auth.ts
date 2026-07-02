33df7dd31ac0d7ea365e93403edc70c7f86af8b8
Mattia Mauro <devmatt044@gmail.com>
2026-07-03 01:54:50 +0200

    agigunta secondo account

--- auth.ts
+++ auth.ts
@@ -2,18 +2,23 @@ import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  trustHost: true,
+
   pages: {
     signIn: "/admin/login",
   },
+
   session: {
     strategy: "jwt",
   },
+
   providers: [
     Credentials({
       credentials: {
         email: {},
         password: {},
       },
+
       async authorize(credentials) {
         const email = credentials?.email as string | undefined;
         const password = credentials?.password as string | undefined;
@@ -22,18 +27,34 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
           return null;
         }
 
-        if (
-          email === process.env.ADMIN_EMAIL &&
-          password === process.env.ADMIN_PASSWORD
-        ) {
-          return {
-            id: "admin",
+        const admins = [
+          {
+            id: "admin-1",
             name: "AFLUXURYCAR Admin",
-            email,
-          };
+            email: process.env.ADMIN_EMAIL,
+            password: process.env.ADMIN_PASSWORD,
+          },
+          {
+            id: "admin-2",
+            name: "AFLUXURYCAR Admin 2",
+            email: process.env.ADMIN_EMAIL2,
+            password: process.env.ADMIN_PASSWORD2,
+          },
+        ];
+
+        const admin = admins.find(
+          (admin) => admin.email === email && admin.password === password
+        );
+
+        if (!admin?.email) {
+          return null;
         }
 
-        return null;
+        return {
+          id: admin.id,
+          name: admin.name,
+          email: admin.email,
+        };
       },
     }),
   ],
